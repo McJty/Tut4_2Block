8d91e621ac3e7c0ccf73ac765b1983450b57b034
McJty <mcjty1@gmail.com>
2023-06-17 17:00:34 +0200

    Better loot table

--- src/main/java/com/mcjty/tut2block/datagen/TutLootTables.java
+++ src/main/java/com/mcjty/tut2block/datagen/TutLootTables.java
@@ -3,6 +3,7 @@ package com.mcjty.tut2block.datagen;
 import com.mcjty.tut2block.Registration;
 import com.mcjty.tut2block.Tutorial2Block;
 import com.mcjty.tut2block.blocks.ComplexBlockEntity;
+import com.mcjty.tut2block.blocks.ProcessorBlockEntity;
 import net.minecraft.data.loot.packs.VanillaBlockLoot;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
@@ -11,6 +12,7 @@ import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.entries.DynamicLoot;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.entries.LootPoolSingletonContainer;
 import net.minecraft.world.level.storage.loot.functions.CopyNameFunction;
 import net.minecraft.world.level.storage.loot.functions.CopyNbtFunction;
 import net.minecraft.world.level.storage.loot.functions.SetContainerContents;
@@ -26,8 +28,8 @@ public class TutLootTables extends VanillaBlockLoot {
     @Override
     protected void generate() {
         dropSelf(Registration.SIMPLE_BLOCK.get());
-        createStandardTable(Registration.COMPLEX_BLOCK.get(), Registration.COMPLEX_BLOCK_ENTITY.get());
-        createStandardTable(Registration.PROCESSOR_BLOCK.get(), Registration.PROCESSOR_BLOCK_ENTITY.get());
+        createStandardTable(Registration.COMPLEX_BLOCK.get(), Registration.COMPLEX_BLOCK_ENTITY.get(), ComplexBlockEntity.ITEMS_TAG);
+        createStandardTable(Registration.PROCESSOR_BLOCK.get(), Registration.PROCESSOR_BLOCK_ENTITY.get(), ProcessorBlockEntity.ITEMS_INPUT_TAG, ProcessorBlockEntity.ITEMS_OUTPUT_TAG);
     }
 
     @Override
@@ -38,16 +40,17 @@ public class TutLootTables extends VanillaBlockLoot {
                 .collect(Collectors.toList());
     }
 
-    private void createStandardTable(Block block, BlockEntityType<?> type) {
+    private void createStandardTable(Block block, BlockEntityType<?> type, String... tags) {
+        LootPoolSingletonContainer.Builder<?> lti = LootItem.lootTableItem(block);
+        lti.apply(CopyNameFunction.copyName(CopyNameFunction.NameSource.BLOCK_ENTITY));
+        for (String tag : tags) {
+            lti.apply(CopyNbtFunction.copyData(ContextNbtProvider.BLOCK_ENTITY).copy(tag, "BlockEntityTag." + tag, CopyNbtFunction.MergeStrategy.REPLACE));
+        }
+        lti.apply(SetContainerContents.setContents(type).withEntry(DynamicLoot.dynamicEntry(new ResourceLocation("minecraft", "contents"))));
+
         LootPool.Builder builder = LootPool.lootPool()
                 .setRolls(ConstantValue.exactly(1))
-                .add(LootItem.lootTableItem(block)
-                        .apply(CopyNameFunction.copyName(CopyNameFunction.NameSource.BLOCK_ENTITY))
-                        .apply(CopyNbtFunction.copyData(ContextNbtProvider.BLOCK_ENTITY)
-                                .copy(ComplexBlockEntity.ITEMS_TAG, "BlockEntityTag." + ComplexBlockEntity.ITEMS_TAG, CopyNbtFunction.MergeStrategy.REPLACE))
-                        .apply(SetContainerContents.setContents(type)
-                                .withEntry(DynamicLoot.dynamicEntry(new ResourceLocation("minecraft", "contents"))))
-                );
+                .add(lti);
         add(block, LootTable.lootTable().withPool(builder));
     }
 
